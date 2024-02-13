9b952028129c2bf67d3d4a1cdfa63c6945fe81bf
David <okekedavid1333@gmail.com>
2024-02-13 17:53:00 +0100

    yes page update

--- src/pages/Yes.jsx
+++ src/pages/Yes.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import Yesimg from '../assets/Yesimg.jpeg' 
+import React, { useEffect, useState } from 'react'
+import Yesimg from '../assets/yes2.jpeg' 
 
 const Yes = () => {
-  return (
-     <div className='flex justify-center items-center h-[100vh] bg-[#f7f7f7]'>
-        <img src={Yesimg} alt="" className='md:w-1/2 w-full' />
+    const [text1Visible, setText1Visible] = useState(false);
+  const [text2Visible, setText2Visible] = useState(false);
+  const [text3Visible, setText3Visible] = useState(false);
 
+  useEffect(() => {
+    // Use setTimeout to control the fading-in sequence
+    const delay = 1000; // 1000 milliseconds (1 second) delay
+
+    const timeout1 = setTimeout(() => {
+      setText1Visible(true);
+    }, delay);
+
+    const timeout2 = setTimeout(() => {
+      setText2Visible(true);
+    }, delay * 2);
+
+    const timeout3 = setTimeout(() => {
+      setText3Visible(true);
+    }, delay * 4);
+
+    return () => {
+      // Clear timeouts to avoid memory leaks
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+      clearTimeout(timeout3);
+    };
+  }, []);
+  return (
+    <div className='bg-black h-screen'>
+    <div className="bg-no-repeat flex justify-center items-center bg-center w-full h-full md:h-screen bg-[url('./assets/yes2.jpeg')] flex-col text-center" >
+            {/* Your component content goes here */}
+            <h1 className={`text-[2rem] text-white ${text1Visible ? 'animate-fade-in' : 'opacity-0'}`}>You for no gree mtcheeew</h1>
+            <h1 className={`text-[1.5rem] text-white ${text2Visible ? 'animate-fade-in' : 'opacity-0'}`}>I have a Surprise for you later tonight</h1>
+            <h1 className={`text-[1.5rem] text-white ${text3Visible ? 'animate-fade-in' : 'opacity-0'}`}>PS: I love you</h1>
+      
+        </div>
     </div>
   )
 }
